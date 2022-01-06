fe62903fd8b8ba4604e2cb2269dfb0efde50fbf9
amanuel <amanuelnobel10@gmail.com>
2022-01-06 11:07:28 +0000

    Upadte: Pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
+    import sys
 
-        sys.argv.pop(0)
-        total = 0
-        for numbers in sys.argv:
-                total = total + int(numbers)
-        print("{:d}".format(total))
+    sys.argv.pop(0)
+    total = 0
+    for numbers in sys.argv:
+        total = total + int(numbers)
+    print("{:d}".format(total))

5889a9a9d516c7218f6f53ae4ac80b3e177b3c41
amanuel <amanuelnobel10@gmail.com>
2022-01-25 19:36:32 +0000

    ADD: classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """ The Square Module.
 
-This module contains a class that defines a square and __init__ method that sets its size with
+This module contains a class that defines a square
+and __init__ method that sets its size with
 and Integer only
-and another method that 'area' that calculates area and setter and getter method the get or sets it
+and another method that 'area' that calculates
+area and setter and getter method the get or sets it
 then prints a square with size 'size' """
+
+
 class Square:
+
     """Defines a square."""
     def __init__(self, size=0):
         """Sets the necessary attributes for the Square object.

ebc409dc1fe77bd06a20a87cc326a1da8e39d77a
Amanuel <amanuelnobel@gmail.com>
2022-02-11 08:20:46 +0000

    Json dumps fun

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """from_json_string module.
-
 Contains a function that returns an object represented by a JSON string.
 """
 import json
@@ -9,6 +8,5 @@ import json
 def from_json_string(my_str):
     """
     Returns an object (Python data structure)
-    represented by a JSON string:.
     """
-    return json.loads(my_str)
+    return json.dumps(my_str)
